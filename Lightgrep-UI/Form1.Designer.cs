649000f4df2becdbd37aef7128a64e15e140907d
sergiomarotco <sergiomarotco@gmail.com>
2023-03-02 01:52:54 +0300

    Добавьте файлы проекта.

---
+++ Lightgrep-UI/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Lightgrep_UI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(176, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Drag / drop file";
+            this.label1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Label1_DragEnter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 70);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(469, 27);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "((http|git|ssh|http(s)|file|\\/?)|(git@[\\w\\.]+))(:(\\/\\/)?)([\\w\\.@\\:/\\-~]+)(\\.git)(" +
+    "\\/)?";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 112);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(54, 20);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Author";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(398, 112);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(83, 20);
+            this.linkLabel2.TabIndex = 4;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "icons8.com";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel2_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "String to be searched";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(137, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(23, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "or";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(291, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "or";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(325, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 29);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Get from clipboard";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 143);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Tag = "grep";
+            this.Text = "LightGrep UI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button3;
+    }
+}
---
+++ Lightgrep-UI/Form1.cs
@@ -0,0 +1,119 @@
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.Text.RegularExpressions;
+
+namespace Lightgrep_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LinkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://github.com/sergiomarotco");
+        }
+
+        private void LinkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://ru.icons8.com"); // открыть рекламную ссылку
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new())
+            {
+                openFileDialog.InitialDirectory = Environment.CurrentDirectory;
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new(fileStream))
+                    {
+                        string fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            Search(filePath, "");
+        }
+
+        private void Search(string file_Path, string text_for_grep)
+        {
+            string text = "";
+            List<string> Matches = new();
+            if (!file_Path.Equals("")) // поиск по файлу
+            {
+                text = File.ReadAllText(file_Path);
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(text_for_grep)) // поиск по тексту
+                {
+                    text = text_for_grep;
+                }
+            }
+
+            var m1 = Regex.Matches(text, textBox1.Text);
+            foreach (Match match in m1.Cast<Match>())
+            {
+                Matches.Add(match.Value);
+            }
+
+            if (Matches.Count > 0)
+            {
+                DateTime dateTime = DateTime.Now;
+                string result = Path.GetTempPath() + "tempgrep " + dateTime.Ticks+ ".txt";
+                if (File.Exists(result))
+                {
+                    File.Delete(result);
+                }
+
+                File.WriteAllLines(result, Matches);
+                Process txt = new();
+                txt.StartInfo.FileName = "notepad.exe";
+                txt.StartInfo.Arguments = result;
+                txt.Start();
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            Search("", Clipboard.GetText());
+        }
+        /// <summary>
+        /// Поиск в тексте из файла полученного при помощи drag and drop
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Label1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data != null)
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+                {
+                    if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                    {
+                        string[] fileList = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                        if (fileList != null)
+                        {
+                            if (fileList.Length == 1)
+                            {
+                                Search(fileList[0], "");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
